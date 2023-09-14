707caa918c82e8da2750609b8e4e544d63be90d0
Yadija <ayuliadi01@gmail.com>
2023-09-14 08:45:18 +0800

    📦refactor: change limit when get api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,28 +2,28 @@ const api = (() => {
   const BASE_URL = 'https://api.jikan.moe/v4';
 
   async function getSeasonNow(page = 1) {
-    const response = await fetch(`${BASE_URL}/seasons/now?page=${page}`);
+    const response = await fetch(`${BASE_URL}/seasons/now?page=${page}&limit=24`);
     const responseJson = await response.json();
 
     return responseJson;
   }
 
   async function getSeasonUpcoming(page = 1) {
-    const response = await fetch(`${BASE_URL}/seasons/upcoming?page=${page}`);
+    const response = await fetch(`${BASE_URL}/seasons/upcoming?page=${page}&limit=24`);
     const responseJson = await response.json();
 
     return responseJson;
   }
 
   async function getTopAnime(page = 1) {
-    const response = await fetch(`${BASE_URL}/top/anime?page=${page}`);
+    const response = await fetch(`${BASE_URL}/top/anime?page=${page}&limit=24`);
     const responseJson = await response.json();
 
     return responseJson;
   }
 
   async function getTopManga(page = 1) {
-    const response = await fetch(`${BASE_URL}/top/manga?page=${page}`);
+    const response = await fetch(`${BASE_URL}/top/manga?page=${page}&limit=24`);
     const responseJson = await response.json();
 
     return responseJson;
@@ -39,14 +39,14 @@ const api = (() => {
   async function getBySearch(type, queryParams) {
     const { query = '', page = 1 } = queryParams;
 
-    const response = await fetch(`${BASE_URL}/${type}?q=${query}&page=${page}`);
+    const response = await fetch(`${BASE_URL}/${type}?q=${query}&page=${page}&limit=24`);
     const responseJson = await response.json();
 
     return responseJson;
   }
 
   async function getProducers(page = 1) {
-    const response = await fetch(`${BASE_URL}/producers?page=${page}`);
+    const response = await fetch(`${BASE_URL}/producers?page=${page}&limit=24`);
     const responseJson = await response.json();
 
     return responseJson;
