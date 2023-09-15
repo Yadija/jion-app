a147e90075eff385856ce17f14f6b16c90c96127
Yadija <ayuliadi01@gmail.com>
2023-09-15 11:00:29 +0800

    📦refactor: change name app in navbar to image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,24 @@ import { Link } from 'react-router-dom';
 
 // context
 import { SearchContext } from '../../context/SearchContext.jsx';
+import { ThemeContext } from '../../context/ThemeContext.jsx';
+// images
+import JionBlack from '../../images/jion-black.png';
+import JionWhite from '../../images/jion-white.png';
 // component
 import DarkMode from '../DarkMode/DarkMode.jsx';
 import Drawer from '../Drawer/Drawer.jsx';
 
 const Navbar = () => {
   const { isShowSearchModal, toggleSearchModal } = useContext(SearchContext);
+  const { mode } = useContext(ThemeContext);
 
   return (
     <nav className='background-color-blue text-color-white sticky top-0 z-[10000] flex items-center justify-between px-6 py-3 transition-all duration-1000'>
       <h2 className='text-xl font-bold'>
-        <Link to='/'>Jion</Link>
+        <Link to='/'>
+          <img className='h-8' src={mode === 'dark' ? JionBlack : JionWhite} alt='logo' />
+        </Link>
       </h2>
       <div className='flex items-center gap-3'>
         <button
