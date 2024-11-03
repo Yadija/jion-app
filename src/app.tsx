a091f0824355067f41b6c04326d30536e91f6928
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-11-03 22:29:33 +0800

    ✨feat: implement useRoutes

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 
 // components
 import SearchModal from "./components/modal/search-modal";
@@ -30,25 +30,69 @@ export default function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
 
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/now",
+      element: <NowPage />,
+    },
+    {
+      path: "/upcoming",
+      element: <UpcomingPage />,
+    },
+    {
+      path: "/top",
+      element: <TopPage />,
+    },
+    {
+      path: "/top/anime",
+      element: <TopAnimePage />,
+    },
+    {
+      path: "/top/manga",
+      element: <TopMangaPage />,
+    },
+    {
+      path: "/anime",
+      element: <AnimePage />,
+    },
+    {
+      path: "/manga",
+      element: <MangaPage />,
+    },
+    {
+      path: "/producers",
+      element: <ProducersPage />,
+    },
+    {
+      path: "/anime/:id",
+      element: <DetailAnimePage />,
+    },
+    {
+      path: "/manga/:id",
+      element: <DetailMangaPage />,
+    },
+    {
+      path: "/producer/:id",
+      element: <DetailProducerPage />,
+    },
+    {
+      path: "/favorite/:type",
+      element: <FavoritePage />,
+    },
+    {
+      path: "*",
+      element: <NotFoundPage />,
+    },
+  ]);
+
   return (
     <main className="background-color-white min-h-screen transition-all duration-1000 selection:bg-fun-blue selection:text-soft-peach selection:dark:bg-denim-blue dark:selection:text-baltic-sea">
       {isShowSearchModal && <SearchModal />}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/now" element={<NowPage />} />
-        <Route path="/upcoming" element={<UpcomingPage />} />
-        <Route path="/top" element={<TopPage />} />
-        <Route path="/top/anime" element={<TopAnimePage />} />
-        <Route path="/top/manga" element={<TopMangaPage />} />
-        <Route path="/anime" element={<AnimePage />} />
-        <Route path="/manga" element={<MangaPage />} />
-        <Route path="/producers" element={<ProducersPage />} />
-        <Route path="/anime/:id" element={<DetailAnimePage />} />
-        <Route path="/manga/:id" element={<DetailMangaPage />} />
-        <Route path="/producers/:id" element={<DetailProducerPage />} />
-        <Route path="/favorite/:type" element={<FavoritePage />} />
-        <Route path="/*" element={<NotFoundPage />} />
-      </Routes>
+      {element}
     </main>
   );
 }
