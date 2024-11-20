6c797f64b5e525f9596175190b30a285e680092f
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-11-20 09:37:36 +0800

    📦refactor: implement aspect ratio in card item and adjust cards list

--- src/components/common/card-item.tsx
+++ src/components/common/card-item.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 
+// components
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card as UICard } from "@/components/ui/card";
 // types
 import { Card } from "@/types/card.type";
 
@@ -13,25 +16,29 @@ export default function CardItem({
   rating = "",
 }: CardItemProps) {
   return (
-    <Link to={link}>
-      <section
-        className="select-none overflow-hidden rounded-lg text-soft-peach shadow-md dark:text-baltic-sea"
-        title={title}
-      >
-        <img
-          src={image}
-          alt={title}
-          className={`${
-            rating?.toLowerCase().includes("rx") &&
-            "blur-md" /* filter for nsfw */
-          } ${
-            type === "producers" ? "h-[140px]" : "h-[260px]"
-          } pointer-events-none w-full bg-gradient-to-tl from-gray-300 to-white object-cover object-center`}
-        />
-        <h2 className="line-clamp-1 bg-fun-blue p-1 text-center font-semibold transition-all duration-1000 dark:bg-soft-peach">
-          {title}
-        </h2>
-      </section>
-    </Link>
+    <UICard>
+      <Link to={link}>
+        <section
+          className="select-none overflow-hidden rounded-lg text-soft-peach shadow-md dark:text-baltic-sea"
+          title={title}
+        >
+          <AspectRatio ratio={type === "producer" ? 1 / 1 : 4 / 5} asChild>
+            <img
+              src={image}
+              alt={title}
+              className={`${
+                rating?.toLowerCase().includes("rx") &&
+                "blur-md" /* filter for nsfw */
+              } ${
+                type === "producers" ? "" : ""
+              } pointer-events-none size-full bg-gradient-to-tl from-gray-300 to-white object-cover object-center`}
+            />
+          </AspectRatio>
+          <h2 className="line-clamp-1 bg-fun-blue p-1 text-center font-semibold transition-all duration-1000 dark:bg-soft-peach">
+            {title}
+          </h2>
+        </section>
+      </Link>
+    </UICard>
   );
 }
--- src/components/common/cards-list.tsx
+++ src/components/common/cards-list.tsx
@@ -7,7 +7,7 @@ interface CardsListProps {
 
 export default function CardsList({ data }: CardsListProps) {
   return (
-    <section className="grid gap-x-4 gap-y-6 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+    <section className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
       {data.map((item) => (
         <CardItem key={item.id} {...item} />
       ))}
